8707a9f3c8198069ae5357768012d20dec228a9c
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-09-10 11:56:01 -0300

    Aula0910 - Footer e Navbar

--- nomadelife-front/src/App.jsx
+++ nomadelife-front/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   )
 }
--- nomadelife-front/src/components/NavBar.jsx
+++ nomadelife-front/src/components/NavBar.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from './NavBar.module.css'
 
 const NavBar = () => {
   return (
     <>
-    
+    <nav className={styles.navbar}>
+      <div className={styles.brand}>
+        Nomade <span>DEV</span>
+      </div>
+      <ul className={styles.links_list}>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+        <li>
+          <a href="#">Register</a>
+        </li>
+        <li>
+          <a href="#">New Post</a>
+        </li>
+        <li>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Exit</a>
+        </li>
+      </ul>
+    </nav>
     </>
   )
 }
