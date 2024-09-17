ee52d69903eb8136d1501be62633db88b70b2322
LucasCotrim2003 <lucasdasilvacotrim03@gmail.com>
2024-09-17 12:32:40 -0300

    Aula 0970 - autenticação

--- nomadelife-front/src/App.jsx
+++ nomadelife-front/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
+import Footer from '.components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ nomadelife-front/src/pages/CreatPost/CreatPost.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+const CreatPost = () => {
+    
+  return (
+    <>
+    </>
+  )
+}
+
+export default CreatPost
