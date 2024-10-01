5ae663eab81ea38d321fe6a533e708ba4aa28cdf
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-10-01 10:58:26 -0300

    Aula1001 - Rotas e Comunicação com API Firebase Authentication

--- nomadelife-front/src/App.jsx
+++ nomadelife-front/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate, Form } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import CreatePost from './pages/CreatePost/CreatePost'
+import Login from './pages/Login/Login'
+import Home from './pages/Home/Home'
+import { useState } from 'react'
 
 function App() {
   return (
     <>
-      <NavBar />
-      <CreatePost />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
---
+++ nomadelife-front/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '/logo-quadrada.png'
+
+const Home = () => {
+  return (
+    <div>
+        <img src={logo} alt="Logo da desenvolvedora" />
+    </div>
+  )
+}
+
+export default Home
