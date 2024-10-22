33708e51bf5934badf3de809013a032c3b153fc7
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-10-22 12:06:30 -0300

    Aula1022 - Hooks de Paginação

---
+++ nomadelife-front/src/hooks/useFetchDocument.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import { db } from '../firebase/config'
+import { doc, getDoc } from 'firebase/firestore'
+
+export const useFethDocument = (docCollection, id) => {
+    const [document, setDocument] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    useEffect(() => {
+        const loadDocument = async () => {
+            setLoading(true)
+
+            try{
+                const docRef = await doc(db, docCollection, id)
+                const docSnap = await getDoc(docRef)
+
+                setDocument(docSnap.data())
+            }catch(error){
+                console.error(error)
+                setError(error.message)
+            }
+            setLoading(false)
+        }
+        loadDocument()
+    }, [docCollection, id])
+
+    console.log(document)
+
+    return{
+        document,
+        loading,
+        error
+    }
+}
